2bc5bfaf3b7218ac82b183d2af57682c89e1e42c
Ben Talbot <btalbot@Bens-MacBook-Pro.local>
2013-05-10 14:36:11 -0700

    Missed some the changes on the previous commit

--- facebook/src/com/explodingbarrel/android/UnityAndroidDeviceInfo.java
+++ facebook/src/com/explodingbarrel/android/UnityAndroidDeviceInfo.java
@@ -5,12 +5,19 @@ import android.net.wifi.WifiManager;
 import android.net.wifi.WifiInfo;
 import android.content.*;
 import android.provider.Settings.Secure;
+import android.util.Log;
+
 import com.unity3d.player.*;
 
 class UnityAndroidDeviceInfo 
 {
+	// Debug tag, for logging
+    private static final String TAG = "UnityAndroidDeviceInfo";
+	
 	public static String GetDeviceID()
 	{
+		Log.d(TAG, "DeviceInfo GetDeviceID");
+		
 		String id = "";
 		
 		try
@@ -19,11 +26,16 @@ class UnityAndroidDeviceInfo
 			if( activity != null )
 			{
 				id = Secure.getString( activity.getContentResolver(), Secure.ANDROID_ID);
-			}	
+				Log.d(TAG, "DeviceInfo : Success - id = " + id );
+			}
+			else
+			{
+				Log.d(TAG, "DeviceInfo : Failed - Couldn't get the current activity from Unity");
+			}
 		}
 		catch( Exception e )
 		{
-			
+			Log.d(TAG, "DeviceInfo : Failed - " + e.getMessage() );
 		}
 		
 		return id;
@@ -31,6 +43,8 @@ class UnityAndroidDeviceInfo
 	
 	public static String WifiMacAddress()
 	{
+		Log.d(TAG, "DeviceInfo WifiMacAddress");
+		
 		String mac = "";
 		
 		try
@@ -41,11 +55,17 @@ class UnityAndroidDeviceInfo
 				WifiManager wifiMan = (WifiManager) activity.getSystemService( Context.WIFI_SERVICE );
 				WifiInfo wifiInf = wifiMan.getConnectionInfo();
 				mac = wifiInf.getMacAddress();
-			}	
+				
+				Log.d(TAG, "DeviceInfo : Success - mac address = " + mac );
+			}
+			else
+			{
+				Log.d(TAG, "DeviceInfo : Failed - Couldn't get the current activity from Unity");
+			}
 		}
 		catch( Exception e )
 		{
-			
+			Log.d(TAG, "DeviceInfo : Failed - " + e.getMessage() );
 		}
 		
 		return mac;
